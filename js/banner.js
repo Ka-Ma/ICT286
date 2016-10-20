ad9ec0bb8c3ac24ddf904654ce0cb934dc261e21
Katherine Mann <kkjayne@gmail.com>
2016-10-20 17:06:15 +0800

    some sort of banner happening, needs tweaking

--- js/banner.js
+++ js/banner.js
@@ -7,5 +7,5 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 3000); // Change image every 3 seconds
+    setTimeout(carousel, 10000); // Change image every 10 seconds
 }
