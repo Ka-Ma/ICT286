2c9a34d7abdb6ad057ea9e383d2173a189c92d2e
Katherine Mann <kkjayne@gmail.com>
2016-10-22 07:14:03 +0800

    basic outline

--- js/home.js
+++ js/home.js
@@ -7,5 +7,10 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 10000); // Change image every 10 seconds
+    setTimeout(carousel, 3000); // Change image every 3 seconds
+}
+
+function loadAboutUs() {
+	console.log("Loading about us");
+	document.getElementById("aboutUs-page").innerHTML = "<p>I have loaded the About Us page</p>";
 }
