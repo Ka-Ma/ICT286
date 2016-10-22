c05547cc19758217441889efa31c35421bda416b
Katherine Mann <kkjayne@gmail.com>
2016-10-22 10:05:27 +0800

    progress on nav (and I will remember to start a branch)

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 function loadNav() {
-	document.getElementById("navigation").innerHTML = "<ul> <li><a href='javascript:loadHome()'>Home</a></li><li><a href='javascript:loadAboutUs()'>About Us</a></li><li><a href='javascript:loadSearch()'>Search</a></li><li><a href='javascript:loadCart()'>Cart</a></li><li style= 'float: right'><a href='javascript:loadLIR()'>Login/Register</a></li></ul>";
+	document.getElementById("navigation").innerHTML = "<ul> <li><a id='home' class='active' href='javascript:loadHome()'>Home</a></li><li><a id='aboutUs' href='javascript:loadAboutUs()'>About Us</a></li><li><a id='search'  href='javascript:loadSearch()'>Search</a></li><li><a id='cart'  href='javascript:loadCart()'>Cart</a></li><li style= 'float: right'><a id='login'  href='javascript:loadLIR()'>Login/Register</a></li></ul>";
 }
 
 function carousel() {
@@ -17,4 +17,18 @@ function carousel() {
 function loadAboutUs() {
 	console.log("Loading about us");
 	document.getElementById("aboutUs-page").innerHTML = "<p>I have loaded the About Us page</p>";
+	
+	updateActive("aboutUs");
+}
+
+function updateActive(current) {
+	var oldPg = document.getElementsByClassName("active");
+	var oldPgId = oldPg[0].getAttribute("id");
+	var curPg = document.getElementById(current);
+	
+	console.log(oldPg);
+	console.log(oldPgId);
+		
+	document.getElementById(oldPgId).removeAttribute("class");
+	curPg.setAttribute("class", "active");
 }
